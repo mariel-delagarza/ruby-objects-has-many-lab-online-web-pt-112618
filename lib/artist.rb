af1b782c79170dfb9479cab544b7fc03aa81062e
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-14 15:38:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,13 @@
 #An artist should have many songs and a song should belong to an artist
+
+Class Artist 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+
+  def add_song(song) 
+    
