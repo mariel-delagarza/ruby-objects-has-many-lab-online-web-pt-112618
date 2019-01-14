14ebc21a78ed7189edac2ce670e25897db747e8d
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-14 16:22:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,2 +1,35 @@
 #An artist should have many songs and a song should belong to an artist
 
+Class Artist 
+  
+  attr_accessor :name 
+  @@all = [] 
+  
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+
+  def add_song(song) 
+    song.artist = self 
+  end 
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+  end 
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.song_count 
+    song.all.count 
+  end 
+
+end
