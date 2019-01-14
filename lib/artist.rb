71389e0ef3a17f603015104b2acd1c5a009a15f7
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-14 16:51:54 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+#An artist should have many songs and a song should belong to an artist
+
+class Artist 
+  
+  attr_accessor :name 
+  @@all = [] 
+  
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+
+  def add_song(song) 
+    song.artist = self 
+  end 
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+  end 
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.song_count 
+    Song.all.count 
+  end 
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+#An author should have many posts and a post should belong to an author.
+
+class Post 
+  
+  attr_accessor :title, :author 
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def author_name 
+    author.name if author 
+  end 
+  
+end 
